ece5abefbe59977e5a476674bf6e5a226884a4f8
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-10-30 12:53:30 +0100

    feat(pushNotification): add push notification via pushover

---
+++ src/logger.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+const pushoverCreds = {
+  token: process.env.PUSHOVER_TOKEN,
+  user: process.env.PUSHOVER_USER,
+}
+
+export const write = (message) => {
+  const actualTime = new Date().toLocaleString('fr-FR', {
+    timeZone: 'Europe/Paris',
+  });
+  fs.appendFileSync('./logs.txt', `[${actualTime}] ${message}`);
+}
+
+export const notify = async (title, message) => {
+  await axios.post('https://api.pushover.net/1/messages.json', {
+    ...pushoverCreds,
+    title,
+    message,
+  });
+};
